aba9b45308a6e2317cf0420f86e083e4b9713754
Takeshi Yoshino <tyoshino@chromium.org>
2015-02-19 17:25:27 +0900

    Commit WIP of operation-stream.js

--- reference-implementation/operation-stream.js
+++ reference-implementation/operation-stream.js
@@ -1,80 +1,186 @@
-export function createOperationStream() {
-  var exchange = new Exchange();
+export function createOperationStream(strategy) {
+  var exchange = new Exchange(strategy);
   return {writable: new WritableStream(exchange), readable: new ReadableStream(exchange)};
 }
 
-class Exchange {
+class OperationStatus {
   constructor() {
-    this._writeQueue = [];
-    this._readQueue = [];
-
-    this._abortedPromise = new Promise((resolve, reject) => {
-      this._resolveAbortedPromise = resolve;
-      this._rejectAbortedPromise = reject;
+    this._state = 'waiting';
+    this._result = undefined;
+    this._readyPromise = new Promise((resolve, reject) => {
+      this._resolveReadyPromsie = resolve;
+      this._rejectReadyPromsie = reject;
     });
+  }
+
+  _onCompletion(v) {
+    this._state = 'done';
+    this._result = v;
+    this._resolveReadyPromise();
+  }
+  _onError(e) {
+    this._state = 'errored';
+    this._result = e;
+    this._resolveReadyPromise();
+  }
+
+  get state() {
+    return this._state;
+  }
+  get result() {
+    return this._result;
+  }
+  get ready() {
+    return this._readyPromise;
+  }
+}
+
+class Operation {
+  constructor(type, argument, status) {
+    this._type = type;
+    this._argument = argument;
+    this._status = status;
+  }
+
+  get type() {
+    return this._type;
+  }
+  get argument() {
+    return this._argument;
+  }
+  complete(result) {
+    this._status._onCompletion(result);
+  }
+  error(reason) {
+    this._status._onError(reason);
+  }
+}
+
+class Exchange {
+  constructor(strategy) {
+    this._queue = [];
+    this._queueSize = 0;
+
+    this._window = 0;
+
+    this._writableState = undefined;
+    this._updateWritableState();
+    this._initWritableReadyPromise();
+    this._cancelOperation = undefined;
     this._cancelledPromise = new Promise((resolve, reject) => {
       this._resolveCancelledPromise = resolve;
-      this._rejectCancelledPromise = reject;
     });
 
-    this._state = 'normal';
+    this._readableState = 'waiting';
+    this._readableReadyPromise = new Promise((resolve, reject) => {
+      this._resolveReadableReadyPromise = resolve;
+    });
+    this._abortedPromise = new Promise((resolve, reject) => {
+      this._resolveAbortedPromise = resolve;
+    });
   }
 
-  get aborted() {
-    return this._abortedPromise;
+  _initWritableReadyPromise() {
+    this._writableReadyPromise = new Promise((resolve, reject) => {
+      this._resolveWritableReadyPromise = resolve;
+    });
   }
 
-  get cancelled() {
-    return this._cancelledPromise;
+  _errorAndClearQueue(reason) {
+    for (var i = 0; i < this._queue.length; ++i) {
+      var entry = this._queue[i];
+      entry.error(reason);
+    }
+    this._queue = [];
   }
 
   // Writable side interfaces
 
-  write(value) {
-    if (this._state === 'closed') {
+  get writableState() {
+    return this._writableState;
+  }
+  get writableReady() {
+    return this._writableReadyPromise;
+  }
+  get cancelOperation() {
+    return this._cancelOperation;
+  }
+  get cancelled() {
+    return this._cancelledPromise;
+  }
+
+  _checkWritablePrecondition() {
+    if (this._writableState === 'closed') {
       return Promise.reject(new TypeError('already closed'));
     }
-    if (this._state === 'aborted') {
+    if (this._writableState === 'aborted') {
       return Promise.reject(new TypeError('already aborted'));
     }
-    if (this._state === 'cancelled') {
+    if (this._writableState === 'cancelled') {
       return Promise.reject(new TypeError('cancelled'));
     }
+    return undefined;
+  }
 
-    return new Promise((resolve, reject) => {
-      const writeEntry = {type: 'data', value, resolve, reject};
-      if (this._readQueue.length > 0) {
-        // Assert: this._writeQueue.length === 0
-        const readEntry = this._readQueue.shift();
-        readEntry.resolve(writeEntry);
-      } else {
-        this._writeQueue.push(writeEntry);
-      }
-    });
+  _updateWritableState() {
+    if (this._writableState === 'closed' ||
+        this._writableState === 'aborted' ||
+        this._writableState === 'cancelled') {
+      return;
+    }
+
+    const shouldApplyBackpressure = this._strategy.shouldApplyBackpressure(this._queueSize);
+    if (shouldApplyBackpressure && this._writableState === 'writable') {
+      this._writableState = 'waiting';
+      this._initWritableReadyPromise();
+    } else if (!shouldApplyBackpressure && this._writableState === 'waiting') {
+      this._writableState = 'writable';
+      this._resolveWritableReadyPromise();
+    }
+  }
+
+  write(argument) {
+    const checkResult = this._checkWritePrecondition();
+    if (checkResult !== undefined) {
+      return checkResult;
+    }
+
+    if (this._queue.length === 0) {
+      this._readableState = 'readable';
+      this._resolveReadableReadyPromise();
+    }
+
+    const size = this._strategy.size(argument);
+
+    var status = new OperationStatus();
+    this._queue.push({value: new Operation('data', status, argument), size});
+    this._queueSize += size;
+
+    this._updateWritableState();
+
+    return status;
   }
 
   close() {
-    if (this._state === 'closed') {
-      return Promise.reject(new TypeError('already closed'));
-    }
-    if (this._state === 'aborted') {
-      return Promise.reject(new TypeError('already aborted'));
-    }
-    if (this._state === 'cancelled') {
-      return Promise.reject(new TypeError('cancelled'));
+    const checkResult = this._checkWritePrecondition();
+    if (checkResult !== undefined) {
+      return checkResult;
     }
 
     this._state = 'closed';
 
-    return new Promise((resolve, reject) => {
-      const writeEntry = {type: 'close', resolve, reject};
-      if (this._readQueue.length > 0) {
-        const readEntry = this._readQueue.shift();
-        readEntry.resolve(writeEntry);
-      } else {
-        this._writeQueue.push(writeEntry);
-      }
-    });
+    this._writableState = 'closed';
+
+    if (this._queue.length === 0) {
+      this._readableState = 'readable';
+      this._resolveReadableReadyPromise();
+    }
+
+    this._strategy = undefined;
+
+    const status = new OperationStatus();
+    this._queue.push({value: new Operation('close', undefined, status), size: 0});
+    return status;
   }
 
   abort(reason) {
@@ -87,67 +193,82 @@ class Exchange {
 
     this._state = 'aborted';
 
-    this._rejectAndClearWriteQueue(new TypeError('aborted'));
-    this._rejectAndClearReadQueues(new TypeError('aborted'));
+    if (this._queue.length === 0) {
+      this._readableState = 'readable';
+      this._resolveReadableReadyPromise();
+    }
 
-    return new Promise((resolve, reject) => {
-      this._resolveAbortedPromise({reason, resolve, reject});
-    });
+    this._errorAndClearQueue(new TypeError('aborted'));
+
+    this._resolveAbortedPromise();
+
+    this._strategy = undefined;
+
+    const status = new OperationStatus();
+    this._queue.push({value: new Operation('abort', reason, status), size: 0});
+    return status;
   }
 
   // Readable side interfaces.
 
-  read() {
+  get window() {
+    return this._window;
+  }
+  set window(v) {
+    this._strategy.onWindowUpdate(v);
+    this._window = v;
+    this._updateWritableState();
+  }
+  get readableState() {
+    return this._readableState;
+  }
+  get readableReady() {
+    return this._readableReadyPromise;
+  }
+  get aborted() {
+    return this._abortedPromise;
+  }
+
+  _checkReadPrecondition() {
     if (this._state === 'cancelled') {
       return Promise.reject(new TypeError('already cancelled'));
     }
     if (this._state === 'aborted') {
       return Promise.reject('aborted');
     }
+    return undefined;
+  }
 
-    if (this._writeQueue.length > 0) {
-      // Assert: this._readQueue.length === 0
-      const writeEntry = this._writeQueue.shift();
-      return Promise.resolve(writeEntry);
-    } else {
-      return new Promise((resolve, reject) => {
-        this._readQueue.push({resolve, reject});
-      });
+  read() {
+    const checkResult = this._checkReadPrecondition();
+    if (checkResult !== undefined) {
+      return checkResult;
     }
+
+    if (this._queue.length === 0) {
+      throw new TypeError('not readable');
+    }
+
+    const entry = this._queue.shift();
+    this._queueSize -= entry.size;
+    this._updateWritableState();
+    return entry.value;
   }
 
   cancel(reason) {
-    if (this._state === 'cancelled') {
-      return Promise.reject(new TypeError('already cancelled'));
-    }
-    if (this._state === 'aborted') {
-      return Promise.reject(new TypeError('aborted'));
+    const checkResult = this._checkReadPrecondition();
+    if (checkResult !== undefined) {
+      return checkResult;
     }
 
     this._state = 'cancelled';
 
-    this._rejectAndClearWriteQueue(new TypeError('cancelled'));
-    this._rejectAndClearReadQueues(new TypeError('cancelled'));
+    this._errorAndClearQueue(new TypeError('cancelled'));
 
-    return new Promise((resolve, reject) => {
-      this._rejectCancelledPromise({reason, resolve, reject});
-    }
-  }
-
-  _rejectAndClearReadQueue(e) {
-    for (var i = 0; i < this._readQueue.length; ++i) {
-      var entry = this._readQueue[i];
-      entry.reject(e);
-    }
-    this._readQueue = [];
-  }
-
-  _rejectAndClearWriteQueue(e) {
-    for (var i = 0; i < this._writeQueue.length; ++i) {
-      var entry = this._writeQueue[i];
-      entry.reject(e);
-    }
-    this._writeQueue = [];
+    const status = new OperationStatus();
+    this._cancelOperation = new Operation('cancel', reason, status);
+    this._resolveCancelledPromise();
+    return status;
   }
 }
 
@@ -158,6 +279,15 @@ class WritableStream {
     this._exchange = exchange;
   }
 
+  get state() {
+    return this._exchange.writableState;
+  }
+  get ready() {
+    return this._exchange.writableReady;
+  }
+  get cancelOperation() {
+    return this._exchange.cancelOperation;
+  }
   get cancelled() {
     return this._exchange.cancelled;
   }
@@ -178,9 +308,22 @@ class ReadableStream {
     this._exchange = exchange;
   }
 
+  get state() {
+    return this._exchange.readableState;
+  }
+  get ready() {
+    return this._exchange.readableReady;
+  }
   get aborted() {
     return this._exchange.aborted;
   }
+  get window() {
+    return this._exchange.window;
+  }
+  set window(v) {
+    this._exchange.window = v;
+  }
+
   read() {
     return this._exchange.read();
   }
